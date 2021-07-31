7a56201436077fb51da6da603d9b531df4707e54
lucifer20042004 <85445084+lucifer20042004@users.noreply.github.com>
2021-07-31 22:50:46 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -12324,7 +12324,7 @@ keyboard.inline_keyboard = {
 {{text = '00:00', url="t.me/SOURCE_WOOLF"}}, 
 } 
 local msg_id = msg.id_/2097152/0.5 
-https.request("https://api.telegram.org/bot"..token..'/sendDocument?chat_id=' .. msg.chat_id_ .. '&Document=https://t.me/Document_AV/'..ght..'&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
+https.request("https://api.telegram.org/bot"..token..'/sendDocument?chat_id=' .. msg.chat_id_ .. '&document=https://t.me/Document_AV/'..ght..'&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
 end
 if text == "كلمني" then
 rpl = {"ها هلاو","انطق","كول"};
